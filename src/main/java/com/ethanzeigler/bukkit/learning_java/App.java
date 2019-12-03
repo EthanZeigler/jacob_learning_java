01f5bc2376863fc119d14dd8700c6ede9c6d2138
Ethan Zeigler <ethanzeigler@gmail.com>
2019-12-02 19:25:15 -0500

    initial commit

---
+++ src/main/java/com/ethanzeigler/bukkit/learning_java/App.java
@@ -0,0 +1,15 @@
+package com.ethanzeigler.bukkit.learning_java;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Learning extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
